ae62810c01604d9dcefe0bd94faf8649c0a19a9c
albukhary <lazizbekkahramonov@gmail.com>
2021-06-18 21:47:35 +0500

    added my own solutions to 01_getting-started

--- 01_getting-started/03_UTF-8/main.go
+++ 01_getting-started/03_UTF-8/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 60; i < 122; i++ {
-		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+		//fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+		fmt.Printf("%d \t ,%b \t ,%#x \t %q \n",i,i,i,i)
 	}
 }
