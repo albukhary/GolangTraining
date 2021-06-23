69717052f4ff821c148cf66cffcbb82118a4fe0c
albukhary <lazizbekkahramonov@gmail.com>
2021-06-23 18:57:25 +0500

    added my own solutions and also Palindrome

--- 22_go-routines/12_channels_pipeline/03_challenge-description/main.go
+++ 22_go-routines/12_channels_pipeline/03_challenge-description/main.go
@@ -1,28 +1,32 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	c := factorial(4)
-	for n := range c {
-		fmt.Println(n)
-	}
-}
-
-func factorial(n int) chan int {
-	out := make(chan int)
-	go func() {
-		total := 1
-		for i := n; i > 0; i-- {
-			total *= i
-		}
-		out <- total
-		close(out)
-	}()
-	return out
-}
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	c := factorial(4,5,2,6,7)
+//	for n := range c {
+//		fmt.Println(n)
+//	}
+//}
+//
+//func factorial(numbers ...int) chan int {
+//	out := make(chan int)
+//
+//		go func() {
+//			for n := range numbers {
+//				total := 1
+//				for i := n; i > 0; i-- {
+//					total *= i
+//				}
+//				out <- total
+//			}
+//			close(out)
+//		}()
+//
+//	return out
+//}
 
 /*
 CHALLENGE #1:
@@ -34,3 +38,45 @@ POST TO DISCUSSION:
 -- eg, what insights did you have which helped you understand working with concurrency?
 -- Post your answer, and read other answers, here: https://goo.gl/uJa99G
 */
+package main
+
+import "fmt"
+
+func main(){
+	palindrome(123454321)
+}
+func palindrome(n int) bool {
+	//12321
+	var digits []int
+	i:=0
+	for n>0 {
+		digits= append(digits, n%10)
+		n=n/10
+		i++
+	}
+	length := len(digits)
+	fmt.Println(digits)
+
+	if length%2 ==0{
+		fmt.Println("It is not a palindrome")
+		return false
+	}
+
+	for i:=0;i<length/2;i++{
+		if digits[i]==digits[length-1-i]{
+			fmt.Println("It is a palindrome !")
+			return true
+		}
+	}
+	return false
+}
+/*
+
+PostGres
+Gin
+Swagger
+Redis
+
+Hammasini ishlatib CRUD
+
+ */
